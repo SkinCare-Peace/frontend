fd397f5c2a7e847aad5cb7f9605953affa3a47ae
shu030929 <shu030929@gmail.com>
2024-12-02 04:12:23 +0900

    feat: 보유제품추가 백 연결

--- frontend/lib/routines/routine_create.dart
+++ frontend/lib/routines/routine_create.dart
@@ -28,7 +28,7 @@ class _RoutinePageState extends State<RoutinePage> {
     required int timeMinutes,
     required int moneyWon,
   }) async {
-    final uri = Uri.parse("http://0.00000/routine").replace(queryParameters: {
+    final uri = Uri.parse("http://3.34.5.57/routine").replace(queryParameters: {
       "time_minutes": timeMinutes.toString(),
       "money_won": moneyWon.toString(),
     });
