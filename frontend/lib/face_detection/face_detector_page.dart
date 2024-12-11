d342b84a7c13de7bcb5d64e545a9c632a6fde490
thdudgus <gusthdud527@cau.ac.kr>
2024-12-12 04:39:44 +0900

    feat : 여드름 부위 이름 추가

--- frontend/lib/face_detection/face_detector_page.dart
+++ frontend/lib/face_detection/face_detector_page.dart
@@ -215,7 +215,7 @@ class _FaceDetectorPageState extends State<FaceDetectorPage> {
           if (response.statusCode == 200) {
             print('####### $regionName 데이터 전송 성공');
             print('####### 응답 body: $responseBody');
-            await processServerResponse_acne(responseBody); // 응답 처리 호출
+            await processServerResponse_acne(regionName, responseBody); // 응답 처리 호출
           } else if (response.statusCode == 404) {
             print('####### $regionName 데이터 전송 실패: Not Found (404)');
           } else if (response.statusCode == 400) {
