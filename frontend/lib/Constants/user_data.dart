422e525bf60ccd45999cbc98cded52c2be5d01b5
thdudgus <gusthdud527@cau.ac.kr>
2024-12-13 03:24:31 +0900

    feat : userData 추가, 로그인 네비게이트 수정, 캘린더 연결, 통계 추이 연결

--- frontend/lib/Constants/user_data.dart
+++ frontend/lib/Constants/user_data.dart
@@ -4,11 +4,13 @@ class UserData {
   final String id;
   String skinType;
   List<String> skinConcerns;
-  Map<String, Map<String, int>> avoidIngredients;
-  final DateTime createdAt;
-  final DateTime updatedAt;
-  final String hashedPassword;
+  List<String> avoidIngredients;
+  Map<String, List<String>> ownedCosmetics;
+  String routineId;
   String bsti;
+  final DateTime createdAt;
+  DateTime updatedAt;
+  final String hashedPassword;
   bool sensitive;
   final int age;
 
@@ -19,37 +21,43 @@ class UserData {
     required this.skinType,
     required this.skinConcerns,
     required this.avoidIngredients,
+    required this.ownedCosmetics,
+    required this.routineId,
+    required this.bsti,
     required this.createdAt,
     required this.updatedAt,
     required this.hashedPassword,
-    required this.bsti,
     required this.sensitive,
     required this.age,
   });
 
+  // JSON 데이터를 UserData 객체로 변환하는 factory 생성자
   factory UserData.fromJson(Map<String, dynamic> json) {
     return UserData(
       email: json['email'] ?? '',
-      age: json['age'] ?? '',
+      age: json['age'] ?? 0,
       name: json['name'] ?? '',
       id: json['_id'] ?? '',
       skinType: json['skin_type'] ?? '',
       skinConcerns: json['skin_concerns'] is Iterable
           ? List<String>.from(json['skin_concerns'])
           : [],
-      sensitive: (json['has_sensitive_skin'] is bool)
-          ? json['has_sensitive_skin'] as bool
-          : false, // null 또는 빈 문자열은 false로 처리
-      avoidIngredients: json['avoid_ingredients'] is Map
-          ? Map<String, Map<String, int>>.from(
-              json['avoid_ingredients'].map((key, value) {
+      sensitive: json['has_sensitive_skin'] ?? false,
+      avoidIngredients: json['avoid_ingredients'] is Iterable
+          ? List<String>.from(json['avoid_ingredients'])
+          : [],
+      ownedCosmetics: json['owned_cosmetics'] is Map
+          ? Map<String, List<String>>.from(
+              json['owned_cosmetics'].map((key, value) {
                 return MapEntry(
                   key,
-                  value is Map ? Map<String, int>.from(value) : {},
+                  value is Iterable ? List<String>.from(value) : [],
                 );
               }),
             )
           : {},
+      routineId: json['routine_id'] ?? '',
+      bsti: json['bsti'] ?? '',
       createdAt: json['created_at'] != null
           ? DateTime.parse(json['created_at'])
           : DateTime.now(),
@@ -57,15 +65,14 @@ class UserData {
           ? DateTime.parse(json['updated_at'])
           : DateTime.now(),
       hashedPassword: json['hashed_password'] ?? '',
-      bsti: json['bsti'] ?? '',
     );
   }
 
+  // BSTI를 반환하는 메서드
   String userBSTI() {
     return bsti;
   }
 }
-
 
 bool isUserDataEmpty(UserData userData) {
   return userData.email.isEmpty &&
